50a6cb545e35f01083310e29b49a1794b71be5bb
Peter Ajimoti <oluwaseyipeter450@gmail.com>
2023-03-27 08:04:23 -0700

    added the sub function

---
+++ sub.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) 
+{
+	int x, y;
+
+	if (argc != 3) 
+	{
+		return 1;
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+
+	printf("%d\n", x - y);
+
+	return 0;
+}
+
